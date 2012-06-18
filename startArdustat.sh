d0245a305b5639c0ed803d22eaaeb31cd8629602
Eddie Nolan <eddiejnolan@gmail.com>
2012-06-18 11:46:14 -0400

    Removed deprecated code & moved files

--- startArdustat.sh
+++ startArdustat.sh
@@ -1,43 +1,39 @@
 #!/bin/bash
 
-#echo -e "Pick one serial port number:"
+firstrun=`sed -n '1p' ./config`				#The first line of the file named 'config'
+if [[ "$firstrun" == 'firstrun' ]]; then
+	echo "First run. Installing node.js libraries..."
+	cd ./Ardustat_Control
+	bash ./initializeNodeJS.sh
+	cd ..
+	rm ./config
+	touch ./config
+	echo notfirstrun > ./config
+	echo "Finished installing node.js libraries"
+fi
 
-#ls /dev/ttyACM*
+unamestr=`uname`							#The architecture of the machine
+if [[ "$unamestr" == 'Linux' ]]; then
+	arduinos=`ls -d /dev/* | grep tty[UA][SC][BM]` 				#anything of the form /dev/ttyACM* or /dev/ttyUSB*
+	numofarduinos=`ls -d /dev/* | grep tty[UA][SC][BM] | wc -l` #number of results returned
 
-NUM_PORTS=$(ls -l /dev/ttyACM* | wc -l)
-
-#read port_num
-
-
-#rm /data/db/mongod.lock
-#sleep 1
-#mongod&
-#sleep 1
-cd NodeJS/Ardustat_Viewer
-node view_server.js&
-sleep 1
-google-chrome "http://localhost:8000"&
-sleep 1
-
-cd ../Ardustat_Control
-
-for ((  i = 0 ;  i < $NUM_PORTS;  i++  ))
+fi
+if [[ "$unamestr" == 'Darwin' ]]; then
+	arduinos=`ls -d /dev/* | grep tty.usbmodem*`
+	numofarduinos=`ls -d /dev/* | grep tty.usbmodem* | wc -l`  #Number of results returned
+fi
+if [[ $arduinos == "" ]]; then
+	echo No arduinos found
+	exit
+fi
+cd Ardustat_Control
+if [[ $numofarduinos == "1" ]]; then
+	node expressserver.js $arduinos
+	exit
+fi
+echo You appear to have multiple arduinos connected. Please select one:
+select fname in $arduinos;
 do
-  PORT=/dev/ttyACM$i
-  node expresserver.js $PORT $((8888+$i)) 500&
-  sleep 1
-  google-chrome "http://localhost:$((8888+$i))/cycler"&
-  sleep 1
+	node expressserver.js $fname
+	break;
 done
-
-$SHELL
-
-
-#while true; do
-#    read -p "Start viewer?" yn
-#    case $yn in
-#        [Yy]* ) gnome-terminal -e "mongod"; cd NodeJS/Ardustat_Viewer; gnome-terminal -e "node view_server.js"; cd ../../;  break;;
-#        [Nn]* ) break;;
-#        * ) echo "Please answer yes or no.";;
-#    esac
-#done
