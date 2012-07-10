d9803f3b9866f5320f56017cb4a02302c956ca48
Rigers <rigersqeraj@gmail.com>
2012-07-10 13:42:51 -0400

    Improved startup script so it installs dependencies automatically

--- startArdustat.sh
+++ startArdustat.sh
@@ -11,7 +11,28 @@ if [[ "$unamestr" == 'Darwin' ]]; then
 fi
 
 if [[ "$firstrun" == 'firstrun' ]]; then
-	echo "First run. Installing node.js libraries..."
+	echo "First run."
+	if [[ "$unamestr" == 'Darwin' ]]; then
+		echo "Mac OS X detected. Attempting to install dependencies..."
+		homebrewisinstalled=`type -P brew | wc -l | sed -e 's/^[ \t]*//'`
+		if [[ "$homebrewisinstalled" == '0' ]]; then
+			echo "Homebrew is not yet installed. Installing homebrew..."
+			/usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)" #installs homebrew
+		fi
+		nodejsisinstalled=`type -P node | wc -l | sed -e 's/^[ \t]*//'`
+		if [[ "$nodejsisinstalled" == '0' ]]; then
+			echo "Node.JS is not yet installed. Installing Node.JS..."
+			brew install node
+		fi
+		mongodbinstalled=`type -P mongo | wc -l | sed -e 's/^[ \t]*//'`
+		if [[ "$mongodbisinstalled" == '0' ]]; then
+			echo "MongoDB is not yet installed. Installing MongoDB..."
+			brew install mongodb
+		fi
+		echo "All dependencies are installed."
+	fi
+	
+	echo "Installing node.js libraries..."
 	bash ./initializeNodeJS.sh
 	rm ./config.rc
 	touch ./config.rc
