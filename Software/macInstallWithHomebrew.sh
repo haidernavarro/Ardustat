8194c085ad66b6bd80ad696aadb9c16db821dc8d
Eddie Nolan <eddiejnolan@gmail.com>
2012-07-11 11:37:52 -0400

    Removed some of the automatic stuff from startArdustat.sh

---
+++ Software/macInstallWithHomebrew.sh
@@ -0,0 +1,21 @@
+homebrewisinstalled=`type -P brew | wc -l | sed -e 's/^[ \t]*//'`
+if [[ "$homebrewisinstalled" == '0' ]]; then
+	echo "Homebrew is not yet installed. Installing homebrew..."
+	/usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)" #installs homebrew
+fi
+nodejsisinstalled=`type -P node | wc -l | sed -e 's/^[ \t]*//'`
+if [[ "$nodejsisinstalled" == '0' ]]; then
+	echo "Node.JS is not yet installed. Installing Node.JS..."
+	brew install node
+fi
+mongodbisinstalled=`type -P mongo | wc -l | sed -e 's/^[ \t]*//'`
+if [[ "$mongodbisinstalled" == '0' ]]; then
+	echo "MongoDB is not yet installed. Installing MongoDB..."
+	brew install mongodb
+fi
+avrdudeisinstalled=`type -P avrdude | wc -l | sed -e 's/^[ \t]*//'`
+if [[ "$avrdudeisinstalled" == '0' ]]; then
+	echo "AVRDUDE is not yet installed. Installing AVRDUDE..."
+	brew install avrdude
+fi
+echo "All dependencies are installed."
--- startArdustat.sh
+++ startArdustat.sh
@@ -12,28 +12,29 @@ fi
 if [[ "$firstrun" == 'firstrun' ]]; then
 	echo "First run."
 	if [[ "$unamestr" == 'Darwin' ]]; then
-		echo "Mac OS X detected. Attempting to install dependencies..."
+		echo "Mac OS X detected."
 		homebrewisinstalled=`type -P brew | wc -l | sed -e 's/^[ \t]*//'`
 		if [[ "$homebrewisinstalled" == '0' ]]; then
-			echo "Homebrew is not yet installed. Installing homebrew..."
-			/usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)" #installs homebrew
+			echo "Homebrew is not yet installed."
+			exit 1;
 		fi
 		nodejsisinstalled=`type -P node | wc -l | sed -e 's/^[ \t]*//'`
 		if [[ "$nodejsisinstalled" == '0' ]]; then
-			echo "Node.JS is not yet installed. Installing Node.JS..."
-			brew install node
+			echo "Node.JS is not yet installed."
+			exit 1;
 		fi
 		mongodbisinstalled=`type -P mongo | wc -l | sed -e 's/^[ \t]*//'`
 		if [[ "$mongodbisinstalled" == '0' ]]; then
-			echo "MongoDB is not yet installed. Installing MongoDB..."
-			brew install mongodb
+			echo "MongoDB is not yet installed."
+			exit 1;
 		fi
 		avrdudeisinstalled=`type -P avrdude | wc -l | sed -e 's/^[ \t]*//'`
 		if [[ "$avrdudeisinstalled" == '0' ]]; then
-			echo "AVRDUDE is not yet installed. Installing AVRDUDE..."
-			brew install avrdude
+			echo "AVRDUDE is not yet installed."
+			exit 1;
 		fi
 		echo "All dependencies are installed."
+
 	fi
 	
 	echo "Installing node.js libraries..."
@@ -65,10 +66,7 @@ fi
 if [[ $numofarduinos == "1" ]]; then
 	needsfirmware=`node detectIfFirmwareLoaded.js $arduinos`
 	if [[ $needsfirmware == "nofirmware" ]]; then
-		echo "Arduino does not appear to be loaded with firmware. Loading firmware with AVRDUDE..."
-		cd ../Firmware/avrdude
-		bash uploadFirmware.sh $arduinos
-		cd ../../Software
+		echo "Arduino on $arduinos does not appear to be loaded with firmware. Please upload the firmware to the ardustat."
 	fi
 	node expressserver.js $arduinos $1 $2 $3
 	exit
@@ -78,10 +76,7 @@ select fname in $arduinos;
 do
 	needsfirmware=`node detectIfFirmwareLoaded.js $fname`
 	if [[ $needsfirmware == "nofirmware" ]]; then
-		echo "Arduino does not appear to be loaded with firmware. Loading firmware with AVRDUDE..."
-		cd ../Firmware/avrdude
-		bash uploadFirmware.sh $fname
-		cd ../../Software
+		echo "Arduino on $fname does not appear to be loaded with firmware. Please upload the firmware to the ardustat."
 	fi
 	node expressserver.js $fname $1 $2 $3
 	break;
