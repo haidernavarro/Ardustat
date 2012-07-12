d58db8537613a5d4924d828e3c820bfb475f433d
Eddie Nolan <eddiejnolan@gmail.com>
2012-07-12 15:36:15 -0400

    Updated fileinfo

--- Software/initializeNodeJS.bat
+++ Software/initializeNodeJS.bat
@@ -1,4 +1,3 @@
-del .\node_modules\
 npm install express@2.5.10 socket.io serialport2 mongoskin
 REM Set Windows PATH to include MongoDB utilities (needed for mongoexport)
 path=%PATH%;C:\mongodb\bin
