326eac69d293119d65872e2844516fd51a101a79
Eddie Nolan <eddiejnolan@gmail.com>
2012-07-17 17:44:53 -0400

    Removed OS check in connectToSerial()

--- Windows/Software/expressserver.js
+++ Windows/Software/expressserver.js
@@ -806,28 +806,15 @@ function calibrate_step()
 //************************
 
 function connectToSerial() {
-	if (os.platform() == "darwin" || os.platform() == "linux") {
-		//Operating system is linux or OS X
-		var serialport = require("serialport")
-		var SerialPort = require("serialport").SerialPort
-		var serialPort = new SerialPort(process.argv[2],{baudrate:57600,parser:serialport.parsers.raw});
-	}
-	else if (os.platform().substring(0,3) == "win") {
-		//Operating system is Windows
-		var SerialPort = require("serialport2").SerialPort
-		var serialPort = new SerialPort();
-		serialPort.open(process.argv[2], {
-			baudRate: 57600,
-			dataBits: 8,
-			parity: 'none',
-			stopBits: 1,
-			flowControl: false
-		});
-	}
-	else {
-		console.log("Your operating system isn't supported.")
-		process.exit(0)
-	}
+	var SerialPort = require("serialport2").SerialPort
+	var serialPort = new SerialPort();
+	serialPort.open(process.argv[2], {
+		baudRate: 57600,
+		dataBits: 8,
+		parity: 'none',
+		stopBits: 1,
+		flowControl: false
+	});
 	serialPort.on("data", dataParser);
 	return serialPort
 }
